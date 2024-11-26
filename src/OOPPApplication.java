ddc713be3ff618d40192a3e37e2fbf01b3848e58
jaehwanl705 <jaehwanl705@gmail.com>
2024-11-26 18:57:44 +0900

    db connector,register complete will make manageEmp

--- src/OOPPApplication.java
+++ src/OOPPApplication.java
@@ -1,9 +1,23 @@
-import frames.jumo.CreateRequestFrame;
+import data.DBMS;
+import frames.hwan.Login;
+import frames.hwan.NewMember;
+
+import javax.swing.*;
 
 public class OOPPApplication {
+
     public static void main(String[] args) {
-        CreateRequestFrame f = new CreateRequestFrame("예산 신고");
-        f.setBounds(0, 0, 1000, 1000);
-        f.show();
+        DBMS.connect();
+        Login win1 = new Login("로그인");
+        win1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        win1.setSize(670, 350);
+        win1.setLocation(100, 200);
+        win1.setVisible(true);
+
+        NewMember win2 = new NewMember("회원가입");
+        win2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        win2.setSize(370, 400);
+        win2.setLocation(800, 200);
+        win2.setVisible(true);
     }
 }
--- src/data/DBMS.java
+++ src/data/DBMS.java
@@ -1,27 +1,48 @@
 package data;
 
-import data.mpo.Employee;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.List;
+import java.sql.*;
 
 public class DBMS {
-    private Connection c;
+    private static Connection c;
+    public static Statement DB;
 
-    public void connect(){
+    public static void connect() {
         try {
+            Class.forName(Literals. DB_DRIVER);
             c = DriverManager.getConnection(Literals.DB_SERVER, Literals.DB_USERID, Literals.DB_USERPW);
-            System.out.println("MySQL Connected!");
+            DB = c.createStatement();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void close(){
+    public static ResultSet select(String sql) {
+
+        Statement stmt;
+        stmt = DB;
         try {
-            if (c!=null){
+            return stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void insert(String sql){ //혹은 excute로 하고 입력을 insert, select등을 받아서 if로 하는것도?
+        Statement stmt;
+        stmt = DB;
+        try{
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void close() {
+        try {
+            if (c != null) {
                 c.close();
                 c = null;
                 System.out.println("MySQL Closed.");
