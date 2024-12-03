4a543033a5368e972f9a0d02086e17f026bbda9f
jumo77 <jumo.kang77@gmail.com>
2024-12-03 13:35:59 +0900

    a

--- src/frames/jumo/BudgetResuestFrame.java
+++ src/frames/jumo/BudgetResuestFrame.java
@@ -3,13 +3,19 @@ package frames.jumo;
 import components.BudgetListItem;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class BudgetResuestFrame extends JFrame {
     public BudgetResuestFrame(){
         setTitle("예산 사용 내역");
 
+        JPanel list = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
         BudgetListItem head = new BudgetListItem
                 (0, "부서", "예산액", "사유", "날짜", "결과");
+
+        java.util.List<BudgetListItem> budgetList = new ArrayList<>();
 
     }
 }
